11b71014937c8fca0b67ce8f584e31306360605b
John Verwolf <jdverwolf@gmail.com>
2018-11-22 17:39:09 -0800

    Add comments

--- main/main.go
+++ main/main.go
@@ -5,28 +5,43 @@ import (
     "strconv"
 )
 
+/*
+The code in this main function is to test the causal relationships shown in
+the Diagram.png, attached in this repo.
+ */
 func main() {
+    // First, we’ll initialize vector clocks each of the 3 processes.
     vec := initVectors(3)
     printState(vec)
 
+    // Save the original state of the vectors for a later comparison.
     initialVecs := copyVectors(vec)
 
+    // Show that all vectors are causally concurrent.
     printCompare(vec[0], vec[1])
     printCompare(vec[1], vec[2])
     printCompare(vec[2], vec[0])
 
+    // Send msgs from 2 to 1, and 0 to 1.
     printSendMessage(vec[2], vec[1])
     printSendMessage(vec[0], vec[1])
 
+    // Show that 0 is now causally connected to 1.
     printCompare(vec[0], vec[1])
 
+    // Increment vectors in accordance with work being done.
     printDoWork(vec[0])
     printDoWork(vec[2])
 
+    // Show how 0 and 1 are now causally concurrent.
     printCompare(vec[0], vec[1])
 
+    // Send a message from 0 to 2.
     printSendMessage(vec[0], vec[2])
 
+    // Finally, let’s check the causality in the blue path
+    // in Diagram.png. As shown in the diagram, position N
+    // should depend on position C, but not A.
     printCompare(vec[0], initialVecs[0])
     printCompare(vec[0], initialVecs[2])
 }
